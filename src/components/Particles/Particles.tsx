6c4a3b147dfe5448d16f6ff1f52d0b2e0976c5ba
lawnshogan <Shawnlogan.gis@gmail.com>
2022-11-03 04:09:17 -0600

    React Portfolio

---
+++ src/components/Particles/Particles.tsx
@@ -0,0 +1,16 @@
+import { useCallback } from 'react';
+import { Particles as ReactParticles } from 'react-tsparticles';
+import { loadFull } from 'tsparticles';
+import { ISourceOptions, Engine } from 'tsparticles-engine';
+import particlesConfig from './particles-config.json';
+
+export const Particles: React.FC = () => {
+
+  const initParticles = useCallback(async (engine: Engine) => {
+    await loadFull(engine);
+  }, []);
+
+  return (
+    <ReactParticles init={initParticles} options={particlesConfig as ISourceOptions} />
+  );
+}
